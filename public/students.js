d1baf38389a59a8689732986bd401b6cde7b9d2e
afmire877 <33981371+afmire877@users.noreply.github.com>
2018-12-10 10:55:51 +0300

    Added a modal pop up and styled it

---
+++ public/students.js
@@ -0,0 +1,8 @@
+var close = document.getElementByClassName('close');
+var modal = document.getElementByClassName('modal');
+
+
+close.addEventListener('click', function(){
+	modal.style.display = 'none';
+} )
+
